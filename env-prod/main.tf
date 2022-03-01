80c9c9068df86a862134c53607e709bfda4ec9e9
csyed <smachisty92@gmail.com>
2022-03-02 02:50:53 +0530

    terraform

--- env-prod/main.tf
+++ env-prod/main.tf
@@ -4,6 +4,11 @@ resource "aws_lb" "frontend" {
   load_balancer_type = "application"
   enable_deletion_protection = false
   subnets = ["subnet-023135617420b0936", "subnet-07cabfb07961dbce8"]
+
+  tags = {
+    Name = "backend-${var.ENV}"
+    env = var.ENV
+  }
 }
 
 resource "aws_lb" "backend" {
@@ -12,25 +17,12 @@ resource "aws_lb" "backend" {
   load_balancer_type = "application"
   enable_deletion_protection = false
   subnets = ["subnet-023135617420b0936", "subnet-07cabfb07961dbce8"]
+
+  tags = {
+    Name = "backend-${var.ENV}"
+    env = var.ENV
+  }
 }
 
-module "tags" {
-  count = length(local.ALL_TAGS)
-  source = "git::https://github.com/smachisty92/terraform-tags"
-  TAG_NAME = lookup(element(local.ALL_TAGS,count.index), "name")
-  TAG_VALUE= lookup(element(local.ALL_TAGS,count.index), "value")
-  RESOURCE_ID = [aws_lb.frontend.id]
-}
-locals {
-  ALL_TAGS= [
-    {
-      name = "env"
-      value = var.ENV
-    },
-    {
-      name = "project_name"
-      value = "roboshop"
-    }
-  ]
-}
+
 variable "ENV" {}
