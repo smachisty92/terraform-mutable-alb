8afa282c5667d524ee157d6aa164669d6998b644
csyed <smachisty92@gmail.com>
2022-03-02 02:44:55 +0530

    terraform

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -13,3 +13,25 @@ resource "aws_lb" "backend" {
   enable_deletion_protection = false
   subnets = ["subnet-023135617420b0936", "subnet-07cabfb07961dbce8"]
 }
+
+
+module "tags" {
+  count = length(local.ALL_TAGS)
+  source = "git::https://github.com/smachisty92/terraform-tags"
+  TAG_NAME = lookup(element(local.ALL_TAGS,count.index), "name")
+  TAG_VALUE= lookup(element(local.ALL_TAGS,count.index), "value")
+  RESOURCE_ID = aws_lb.frontend.id
+}
+locals {
+  ALL_TAGS= [
+    {
+      name = "env"
+      value = var.ENV
+    },
+    {
+      name = "project_name"
+      value = "roboshop"
+    }
+  ]
+}
+variable "ENV" {}
