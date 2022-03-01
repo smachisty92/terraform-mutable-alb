3fa7ec3c48aac722eea01160c5fd268e8db5cd0c
csyed <smachisty92@gmail.com>
2022-03-02 02:11:57 +0530

    terraform

--- backend.tf
+++ backend.tf
@@ -3,4 +3,5 @@ resource "aws_lb" "backend" {
   internal           = false
   load_balancer_type = "application"
   enable_deletion_protection = false
+  subnets = ["subnet-023135617420b0936", subnet-07cabfb07961dbce8]
 }
