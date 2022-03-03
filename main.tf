982e3f2239725067330cfe986c9f4060bcb13e11
csyed <smachisty92@gmail.com>
2022-03-04 01:38:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "frontend" {
   internal           = false
   load_balancer_type = "application"
   enable_deletion_protection = false
-  subnets = data.terraform_remote_state.vpc.outputs.SUBNETS_ID
+  subnets = data.terraform_remote_state.vpc.outputs.PUBLIC-SUBNETS_ID
   tags = {
     Name = "frontend-${var.ENV}"
     env = var.ENV
@@ -12,10 +12,10 @@ resource "aws_lb" "frontend" {
 
 #resource "aws_lb" "backend" {
 #  name               = "backend-${var.ENV}"
-#  internal           = false
+#  internal           = true
 #  load_balancer_type = "application"
 #  enable_deletion_protection = false
-#  subnets = ["subnet-023135617420b0936", "subnet-07cabfb07961dbce8"]
+#  subnets = data.terraform_remote_state.vpc.outputs.PRIVATE-SUBNETS_ID
 #  tags = {
 #    Name = "backend-${var.ENV}"
 #    env = var.ENV
