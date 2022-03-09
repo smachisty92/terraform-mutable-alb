66f0c13e62380b8aa5e788de26f6ec97a1b5c62d
csyed <smachisty92@gmail.com>
2022-03-09 14:49:47 +0530

    terraform

--- sg.tf
+++ sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "allow-public" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [0.0.0.0/0]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
 
