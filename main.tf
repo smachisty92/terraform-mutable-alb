34eaef942dce875954608a50995d31bda3c0e38c
csyed <smachisty92@gmail.com>
2022-03-09 14:47:14 +0530

    terraform

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_lb" "frontend" {
   load_balancer_type = "application"
   enable_deletion_protection = false
   subnets = data.terraform_remote_state.vpc.outputs.PUBLIC-SUBNETS_ID
+  security_groups = [aws_security_group.allow-public.name]
   tags = {
     Name = "frontend-${var.ENV}"
     env = var.ENV
